557519436f0f9a49f43321a26f75448cc7ba41dc
Samet <sametozturk973@gmail.com>
2024-09-17 19:33:07 +0300

    Product installed

---
+++ src/api/apiUrl.ts
@@ -0,0 +1,7 @@
+// export const ApiUrl = {
+//     GET_USERS: 'users',
+//     GET_USER: 'users/:id',
+//     CREATE_USER: 'users',
+//     UPDATE_USER: 'users/:id',
+//     DELETE_USER: 'users/:id',
+// };
---
+++ src/utils/filterFlights.tsx
@@ -0,0 +1,1 @@
+// Gelen datayı filtreleme
