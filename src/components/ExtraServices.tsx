c3f57193c7c6d0b18e5e211d7debfabab6039edc
Samet <sametozturk973@gmail.com>
2024-09-19 13:18:32 +0300

    ExtraServices template created

---
+++ src/components/ExtraServices.tsx
@@ -0,0 +1,18 @@
+export default function ExtraServices() {
+  return (
+    <div>
+      <div>
+        <p>Fotoğraf</p>
+        <p>CAR RENTALS</p>
+      </div>
+      <div>
+        <p>Fotoğraf</p>
+        <p>HOTELS</p>
+      </div>
+      <div>
+        <p>Fotoğraf</p>
+        <p>TRAVEL PACKAGES</p>
+      </div>
+    </div>
+  );
+}
