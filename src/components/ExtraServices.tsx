c9bf72faee8703eb1b8a11eb9d54f76965df28fe
Samet <sametozturk973@gmail.com>
2024-09-20 19:11:10 +0300

    Extra services completed

--- src/components/ExtraServices.tsx
+++ src/components/ExtraServices.tsx
@@ -1,17 +1,43 @@
+import { FaCar, FaUmbrellaBeach } from "react-icons/fa";
+import { FaHotel } from "react-icons/fa6";
+
 export default function ExtraServices() {
   return (
-    <div>
-      <div>
-        <p>Fotoğraf</p>
-        <p>CAR RENTALS</p>
+    <div className="flex flex-col gap-6 items-center">
+      <div className="relative cursor-pointer hover:scale-105 transition-transform duration-300">
+        <img
+          src="/assets/images/extraServicesImages/car-image.png"
+          alt="image1"
+          className="max-h-[250px] rounded-2xl"
+        />
+        <div className="absolute bottom-[5%] left-[5%] text-white font-bold text-lg">
+          <FaCar className="w-8 h-8" />
+          <p>CAR RENTALS</p>
+        </div>
       </div>
-      <div>
-        <p>Fotoğraf</p>
-        <p>HOTELS</p>
+
+      <div className="relative cursor-pointer hover:scale-105 transition-transform duration-300">
+        <img
+          src="/assets/images/extraServicesImages/hotel-image.png"
+          alt="image2"
+          className="max-h-[250px] rounded-2xl"
+        />
+        <div className="absolute bottom-[5%] left-[5%] text-white font-bold text-lg">
+          <FaHotel />
+          <p>HOTELS</p>
+        </div>
       </div>
-      <div>
-        <p>Fotoğraf</p>
-        <p>TRAVEL PACKAGES</p>
+
+      <div className="relative cursor-pointer hover:scale-105 transition-transform duration-300">
+        <img
+          src="/assets/images/extraServicesImages/travel-package-image.png"
+          alt="image3"
+          className="max-h-[250px] rounded-2xl"
+        />
+        <div className="absolute bottom-[5%] left-[5%] text-white font-bold text-lg">
+          <FaUmbrellaBeach />
+          <p>TRAVEL PACKAGES</p>
+        </div>
       </div>
     </div>
   );
