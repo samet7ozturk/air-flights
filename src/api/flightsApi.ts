c109eaa40a4b3b8c3bd891b2c9b00bdecf613f65
Samet <sametozturk973@gmail.com>
2024-09-20 17:08:23 +0300

    API configuration

--- src/api/flightsApi.ts
+++ src/api/flightsApi.ts
@@ -3,17 +3,30 @@ import { ApiUrl } from "./apiUrl";
 
 export const fetchFlights = async (
   page: number = 0,
-  includeDelays: boolean = false
+  includeDelays: boolean = false,
+  fromScheduleDate: string = "",
+  toScheduleDate: string = ""
 ) => {
   try {
+    const params: {
+      page: number;
+      includeDelays: boolean;
+      fromScheduleDate: string;
+      toScheduleDate?: string;
+    } = {
+      page,
+      includeDelays,
+      fromScheduleDate,
+    };
+
+    if (toScheduleDate) {
+      params.toScheduleDate = toScheduleDate;
+    }
+
     const response = await InstanceAxios.get(ApiUrl.GET_FLIGHTS, {
-      params: {
-        page,
-        includedelays: includeDelays,
-        sort: "+scheduleTime",
-      },
+      params,
     });
-    return response.data;
+    return response.data.flights;
   } catch (error) {
     console.error("Error fetching flights", error);
     throw error;
--- src/api/instanceAxios.ts
+++ src/api/instanceAxios.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
 export const InstanceAxios = axios.create({
-  baseURL: "https://api.schiphol.nl/public-flights",
-  headers: {
-    resourceversion: "v4",
-    app_id: "431c6a6b",
-    app_key: "682bf9a146fc1b546e31fa6a4d64c2a7",
-  },
+  baseURL: "http://localhost:5555/api",
 });
