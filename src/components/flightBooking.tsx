8ffa3a6a5a25eec863e1b764fa3b3781b4991024
Samet <sametozturk973@gmail.com>
2024-09-20 17:09:02 +0300

    Added filtering by date

--- src/components/flightBooking.tsx
+++ src/components/flightBooking.tsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
 import { FaPlane, FaPlaneDeparture, FaPlaneArrival } from "react-icons/fa";
 import { IoMdCalendar } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store/store";
+import { fetchFlightsThunk } from "../store/thunks/flightsThunk";
 
 const FlightBooking = () => {
   const [selectedOption, setSelectedOption] = useState<
     "round-trip" | "one-way"
   >("round-trip");
 
-  const today = new Date().toISOString().split("T")[0];
+  const [fromScheduleDate, setFromScheduleDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
+  const [returnDate, setReturnDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
+
+  const dispatch = useDispatch<AppDispatch>();
+  const { flights, loading, error } = useSelector(
+    (state: RootState) => state.flights
+  );
+  console.log("Flight Booking Component: ", flights);
+
+  const handleSearchFlights = () => {
+    const toDate = selectedOption === "one-way" ? "" : returnDate;
+    dispatch(fetchFlightsThunk(0, false, fromScheduleDate, toDate));
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="bg-white flex flex-col p-4 rounded-md">
@@ -42,14 +69,14 @@ const FlightBooking = () => {
 
       <div className="flex mb-4 justify-between">
         <div className="flex gap-1">
-          <div className="relative flex-grow">
+          <div className="relative">
             <input
               type="text"
               className="rounded-l-full pl-10 py-2 border-2 border-[#e0e0e0]"
             />
             <FaPlaneDeparture className="absolute top-1/2 transform -translate-y-1/2 left-2 text-[#4a0097] h-6 w-6" />
           </div>
-          <div className="relative flex-grow">
+          <div className="relative">
             <input
               type="text"
               className="rounded-r-full pl-10 py-2 border-2 border-[#e0e0e0]"
@@ -59,18 +86,20 @@ const FlightBooking = () => {
         </div>
 
         <div className="flex gap-1">
-          <div className="relative flex-grow">
+          <div className="relative">
             <input
               type="date"
-              defaultValue={today}
+              value={fromScheduleDate}
+              onChange={(e) => setFromScheduleDate(e.target.value)}
               className="rounded-l-full pl-10 py-2 border-2 border-[#e0e0e0]"
             />
             <IoMdCalendar className="absolute top-1/2 transform -translate-y-1/2 left-2 text-[#4a0097] h-6 w-6" />
           </div>
-          <div className="relative flex-grow">
+          <div className="relative">
             <input
               type="date"
-              defaultValue={today}
+              value={returnDate}
+              onChange={(e) => setReturnDate(e.target.value)}
               disabled={selectedOption === "one-way"}
               className="rounded-r-full pl-10 py-2 border-2 border-[#e0e0e0]"
             />
@@ -80,7 +109,10 @@ const FlightBooking = () => {
       </div>
 
       <div>
-        <button className="rounded-md bg-[#4a0097] text-white px-4 py-2">
+        <button
+          onClick={handleSearchFlights}
+          className="rounded-md bg-[#4a0097] text-white px-4 py-2"
+        >
           Show flights
         </button>
       </div>
