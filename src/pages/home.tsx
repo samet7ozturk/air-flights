b5a65de020e89940d359ce8a0da5f7248b27086e
Samet <sametozturk973@gmail.com>
2024-09-22 14:37:49 +0300

    My flights link edited

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
             </div>
             <div className="flex items-center gap-1">
               <CgProfile />
-              <Link to="/my-fligths">
+              <Link to="/my-flights">
                 <p>My Tickets</p>
               </Link>
             </div>
