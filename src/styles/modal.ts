daf134f1eb9f7dbfd74a5693cff94d9375629d7e
Samet <sametozturk973@gmail.com>
2024-09-22 14:12:49 +0300

    Modal styles created

---
+++ src/styles/modal.ts
@@ -0,0 +1,22 @@
+const modalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    background: "white",
+    border: "none",
+    borderRadius: "8px",
+    padding: "20px",
+    width: "400px",
+    zIndex: 1000,
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    zIndex: 999,
+  },
+};
+
+export default modalStyles;
